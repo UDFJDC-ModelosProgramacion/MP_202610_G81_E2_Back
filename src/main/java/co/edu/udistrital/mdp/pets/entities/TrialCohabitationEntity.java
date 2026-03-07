c24ba743648f6c183231b9c6d90ad635d7a259fc
SpeedyR01 <soriveraf@udistrital.edu.co>
2026-03-06 20:18:54 -0500

    Refactor TrialCohabitationEntity by removing fields
    Removed unused fields and relationships from TrialCohabitationEntity.

--- src/main/java/co/edu/udistrital/mdp/pets/entities/TrialCohabitationEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/TrialCohabitationEntity.java
@@ -1,27 +1,17 @@
 package co.edu.udistrital.mdp.pets.entities;
 
-import java.time.LocalDate;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Data;
-import uk.co.jemos.podam.common.PodamExclude;
+import java.time.LocalDate;
 
 @Data
 @Entity
 public class TrialCohabitationEntity extends AdoptionProcessEntity {
+
     private LocalDate startDate;
+
     private LocalDate endDate;
-    private String periodicNotes;
-    private Boolean isSuccessfull;
 
-    @OneToOne
-    @JoinColumn(name = "adoption_request_id")
-    @PodamExclude
-    private AdoptionRequestEntity adoptionRequest;
+    private Boolean approved;
 
-    @OneToOne(mappedBy = "trialCohabitation")
-    @PodamExclude
-    private AdoptionEntity adoption;
 }
