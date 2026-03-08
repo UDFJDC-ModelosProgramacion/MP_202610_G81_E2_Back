de01a37e638b2f8b05f6f1dd885c58468728e830
Sergio Moreno <slmorenog@udistrital.edu.co>
2026-03-08 16:35:22 -0500

    adding @OneToMany relations in MedicalHistoryEntity

--- src/main/java/co/edu/udistrital/mdp/pets/entities/MedicalHistoryEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/MedicalHistoryEntity.java
@@ -1,9 +1,11 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import java.sql.Date;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -11,11 +13,19 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Data
 @Entity
 public class MedicalHistoryEntity extends BaseEntity {
-          private Date createdDate;
-          private Date lastUpdated;
+    private Date createdDate;
+    private Date lastUpdated;
 
-          @OneToOne
-          @JoinColumn(name = "pet_id")
-          @PodamExclude
-          private PetEntity pet;
+    @OneToOne
+    @JoinColumn(name = "pet_id")
+    @PodamExclude
+    private PetEntity pet;
+
+    @OneToMany(mappedBy = "medicalHistory")
+    @PodamExclude
+    private List<MedicalEventEntity> medicalEvents;
+
+    @OneToMany(mappedBy = "medicalHistory")
+    @PodamExclude
+    private List<VaccineEntryEntity> vaccineEntries;
 }
